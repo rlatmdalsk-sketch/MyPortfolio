8d1a124b4702d0854a4566b35c7d1f37f144c75e
김승민 <rlatmdalsk@naver.com>
2026-02-23 17:09:23 +0900

    Portfolio

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { twMerge } from "tailwind-merge";
+
+function App() {
+
+  return (
+    <>
+        <div className={twMerge("bg-red-700")}>
+            ㅁㅁ
+        </div>
+    </>
+  )
+}
+
+export default App
